019b6138effab231777e875f1f048b6ee017f840
26Kevin <k.patel46@share.epsb.ca>
2022-09-29 10:30:07 -0600

    Event Listeners

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // JavaScript Code
+
+document.getElementById("btn").addEventListener("click", helloButton);
+
+function helloButton() {
+    alert("Hello World");
+    alert("Hello Button");
+}
+
+document.getElementById("heading").addEventListener("click", helloHeading);
+
+function helloHeading() {
+    alert("Hello Heading");
+}
