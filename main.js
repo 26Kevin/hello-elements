94391a64cc426eea80f3686535198612a71ae1b3
26Kevin <k.patel46@share.epsb.ca>
2022-10-03 10:25:54 -0600

    Event Listeners

--- main.js
+++ main.js
@@ -3,12 +3,19 @@
 document.getElementById("btn").addEventListener("click", helloButton);
 
 function helloButton() {
-    alert("Hello World");
-    alert("Hello Button");
+       alert("Hello Button");
 }
 
 document.getElementById("heading").addEventListener("click", helloHeading);
 
 function helloHeading() {
     alert("Hello Heading");
+}
+
+document.getElementById("li1").addEventListener("click", helloListItem);
+document.getElementById("li2").addEventListener("click", helloListItem);
+document.getElementById("li3").addEventListener("click", helloListItem);
+
+function helloListItem() {
+    alert("Hello List Item");
 }
